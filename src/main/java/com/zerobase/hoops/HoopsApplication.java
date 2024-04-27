1fab46f198cb8676e1ccaee41fb0afd34fc2f198
junghoon <111417407+pobiq@users.noreply.github.com>
2024-04-27 15:55:34 +0900

    first commit

---
+++ src/main/java/com/zerobase/hoops/HoopsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.hoops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/hoops/HoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.hoops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
