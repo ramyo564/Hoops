0262c63350ee75ee37ba6761dce3642ace1ad12d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-05-11 14:26:24 +0900

    [리펙토링] jwt 토큰 만료 -> 예외처리 추가 (#47)
    * refactor : 날짜입력 부분 필수로 변경 -> 로직 일부 변경 -> 불필요한 부분 제거
    
    * refactor : jwt 토큰 예외 처리 추가

--- src/main/java/com/zerobase/hoops/security/JwtTokenExtract.java
+++ src/main/java/com/zerobase/hoops/security/JwtTokenExtract.java
@@ -1,6 +1,8 @@
 package com.zerobase.hoops.security;
 
 import com.zerobase.hoops.entity.UserEntity;
+import com.zerobase.hoops.exception.CustomException;
+import com.zerobase.hoops.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,13 +11,17 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class JwtTokenExtract {
-
   public UserEntity currentUser() {
-    Authentication authentication =
-        SecurityContextHolder.getContext().getAuthentication();
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-    return (UserEntity) authentication.getPrincipal();
+    if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal() == null) {
+      throw new CustomException(ErrorCode.EXPIRED_TOKEN);
+    }
+
+    if (authentication.getPrincipal() instanceof UserEntity) {
+      return (UserEntity) authentication.getPrincipal();
+    } else {
+      throw new CustomException(ErrorCode.EXPIRED_TOKEN);
+    }
   }
-
 }
-
